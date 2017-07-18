be0631d77d1fdaa0e50713209100787a0849d13f
Brian Patino <patinobrian@gmail.com>
2017-07-18 16:19:27 -0400

    complete gen header file step

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-// #include <string.h>
+#include <string.h>
 #include <string>
-// #include <iostream>
+#include <iostream>
 
+#include "cmake-build/CMakeTutorialConfig.h"
+
+#define THIS_IS_SOMETHING 10
+
+// Following the tutorial: https://cmake.org/cmake-tutorial/
 int main(int argc, char *argv[]) {
-	printf("Hello C++!");
-	// using namespace std;
-	// cout << "Do stuff";
-	/*if (argc < 2) {
+	using namespace std;
+	if (argc < 2) {
 		std::cout << "Usage: " << argv[0] << " number" << std::endl;
 		return 1;
 	}
 	double inputVal = atof(argv[1]);
 	double outVal = sqrt(inputVal);
-	std::cout << "The square root of " << inputVal << " is " << outVal << std::endl;*/
+	std::cout << "The square root of " << inputVal << " is " << outVal << std::endl;
 	return 0;
 }
