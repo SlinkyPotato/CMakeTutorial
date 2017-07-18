3a265607948ee829b42ffab962e6d8a7eddbe4da
Brian Patino <patinobrian@gmail.com>
2017-07-18 14:21:10 -0400

    add main files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+// #include <string.h>
+#include <string>
+// #include <iostream>
+
+int main(int argc, char *argv[]) {
+	printf("Hello C++!");
+	// using namespace std;
+	// cout << "Do stuff";
+	/*if (argc < 2) {
+		std::cout << "Usage: " << argv[0] << " number" << std::endl;
+		return 1;
+	}
+	double inputVal = atof(argv[1]);
+	double outVal = sqrt(inputVal);
+	std::cout << "The square root of " << inputVal << " is " << outVal << std::endl;*/
+	return 0;
+}
